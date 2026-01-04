a6c439cb18d2ef69c96978c1b9b65b14ed328110
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-04 20:09:34 -0300

    refacto: remove consoles logs

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { treeifyError, z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
