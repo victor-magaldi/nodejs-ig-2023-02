d94f1ab24e8ca4559fa45c6739d928bc9538ba7f
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-04 20:15:07 -0300

    test: configure migration in test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,14 +1,21 @@
 import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+import { beforeEach } from 'node:test'
 
 describe('Transactions Routes', () => {
   beforeAll(async () => {
+    execSync('npm run migrate:latest')
     await app.ready()
+  })
+  beforeEach(() => {
+    execSync('npm run migrate:rollback')
   })
   afterAll(async () => {
     await app.close()
   })
+
   it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
