5a5507fdc75d3d519f2fe2935da3f6bcc8c9ad06
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2026-01-06 21:29:32 -0300

    chore: prepare for deploy in render

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from './app'
 import { env } from '../env'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
+  port: env.PORT
+}).then(() => {
   console.log('HTTP server running')
 })
